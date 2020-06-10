272a15a8dc77ef23c289988a093080dad48f3205
naoki-kkc <naoki2kuchi@gmail.com>
2020-06-10 16:50:14 +0900

    section5 - 70 end

--- section5/udemy-vuejs/src/main.js
+++ section5/udemy-vuejs/src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App.vue' // <= renderに渡す「App」はコレ
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  // ↓ これと同じ
+  // render: function(h){
+  //   return h(App);
+  // }
 }).$mount('#app')
