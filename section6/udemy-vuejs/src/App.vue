a9aa6e6f466daab549712c0067e8d18363136274
naoki-kkc <naoki2kuchi@gmail.com>
2020-06-11 11:38:45 +0900

    section6 - 83 end

--- section6/udemy-vuejs/src/App.vue
+++ section6/udemy-vuejs/src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <LikeHeader></LikeHeader>
+    
+    <!-- [vuecli]パスカルケースで書く -->
+    <!-- <LikeHeader></LikeHeader> -->
+
+    <!-- [vuecli]ケバブケースでも書ける -->
+    <like-header></like-header>
+    
     <LikeNum></LikeNum>
   </div>
 </template>
@@ -12,7 +18,7 @@ import LikeHeader from "./components/LikeHeader.vue";
 export default{
   components:{
     // コンポーネントのローカル登録
-    LikeHeader: LikeHeader,
+    LikeHeader: LikeHeader, // 定義はパスカルケースで行っているが、template内はパスカルケース/ケバブケースを問わない(キャメルケースは想定していないので書かないこと)
   }
 }
 </script>
